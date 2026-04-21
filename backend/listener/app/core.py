58d9264daf5d14e29af88d43ccda2ae1d85f20c0
isaacreisbr23 <isaacreisbr23@gmail.com>
2026-04-21 13:41:11 -0300

    inicio da função de listar comandos no diretorio especifico

--- backend/listener/app/core.py
+++ backend/listener/app/core.py
@@ -1,12 +1,37 @@
 import sounddevice as sd
 import numpy as np
 import speech_recognition as sr
+from pathlib import Path
+
+def listar_comandos():
+
+    documents = Path.home() / "Documents/MeusComandos"
+
+    if not documents.exists():
+        print("Pasta Documents não encontrada")
+        return []
+
+    arquivos = [f.name for f in documents.iterdir() if f.is_file()]
+
+    return arquivos
+
+
+def execute_voice_command(text):
+    
+    if text == None:
+        print("O parametro de execucao nao foi enviado")
+    else:
+        pass
+
 
 def start_listener_app():
 
     fs = 16000  # taxa de amostragem
     seconds = 5
 
+    comandos_encontrados = listar_comandos()
+    print(f"Comandos encontrados {comandos_encontrados}")
+    
     print("Ouvindo...")
 
     audio = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype='int16')
