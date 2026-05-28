a0c75d51525d6c8f04a1b5c1ad4ad8acd104f881
isaacreisbr23 <isaacreisbr23@gmail.com>
2026-05-27 21:07:20 -0300

    username no start da aplicação

--- backend/listener/app/speech_assistant_app.py
+++ backend/listener/app/speech_assistant_app.py
@@ -3,13 +3,14 @@ print(__name__)
 from core import start_listener_app, rotina_60_segundos
 import threading
 import pyttsx3
+import os
 
 print(__name__)
 def falar(texto):
 
     engine = pyttsx3.init(driverName='sapi5')
 
-    engine.setProperty('rate', 200)
+    engine.setProperty('rate', 180)
 
     voices = engine.getProperty('voices')
 
@@ -21,7 +22,7 @@ def falar(texto):
 
     del engine
 
-falar("Aura está ativa")
+falar(f"Aura está ativa. Olá {os.environ.get("USERNAME")}")
 
 thread_rotina = threading.Thread(target=rotina_60_segundos, daemon=True)
 thread_rotina.start()
